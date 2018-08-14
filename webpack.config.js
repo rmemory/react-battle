fd3b19912a56b897c71feab2821a8cd5b676942f
Richard Memory <richardfmemory@gmail.com>
2018-08-14 17:28:42 -0600

    add firebase hosting

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
 
-module.exports = {
+
+var config = {
 	entry: './app/index.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
@@ -10,19 +12,23 @@ module.exports = {
 	},
 
 module: {
-    rules: [
-      { test: /\.(js[x]?)$/, use: 'babel-loader' },
-      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
-    ]
-  },
-  // Without this, a refresh will cause the browser to request from the server
-  devServer: {
-	  historyApiFallback: true
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'app/index.html'
-    })
-  ],
-  mode: "development"
+		rules: [
+			{ test: /\.(js[x]?)$/, use: 'babel-loader' },
+			{ test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
+		]
+	},
+
+	// Without this, a refresh will cause the browser to request from the server
+	devServer: {
+		historyApiFallback: true
+	},
+
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: 'app/index.html'
+		})
+	],
+	mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
 };
+
+module.exports = config;
